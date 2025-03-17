b65705b63ea429421a7e9ce80e661c002735442f
Leonar05 <leonardo.donno05@gmail.com>
2025-03-17 11:51:50 +0100

    Creazione della classe user e pagina della lista

---
+++ FUST.ECommerce/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace FUST.ECommerce.Models
+{
+    public class User
+    {
+        // id, nome, cognome, email, password, data
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Surname { get; set; } = default!;
+        public string EMail { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public DateTime RegistrationDate { get; set; }
+    }
+}
